ab77d28387bf265ed78de4cca1f8c8a8b899f8e4
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-04-03 13:29:42 +0000

    remove const

--- P2.js
+++ P2.js
@@ -1,13 +1,11 @@
 function formatNoms(personnes) {
     return personnes.map(personne => `${personne.prenom} ${personne.nom}`);
   }
-  
   const personnes = [
     { prenom: 'Ahmed', nom: 'ALAMI' },
     { prenom: 'Fatima', nom: 'ALAOUI' },
     { prenom: 'Karim', nom: 'BAGHDADI' }
   ];
-  
   nomsComplets = formatNoms(personnes);
   console.log(nomsComplets);
   
