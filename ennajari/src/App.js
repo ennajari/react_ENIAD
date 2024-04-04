09e4ea35422398c6ca8b117e8c42bf953cc79803
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-04-04 13:57:13 +0000

    Ecrivez un composant qui contient un bouton avec le texte initial « Clicked 0 times », après le clic sur le bouton, incrémenter le compteur de cliques : « Clicked 1 times », « Clicked 2 times »,…

--- ennajari/src/App.js
+++ ennajari/src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
 import './App.css';
+
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+  };
+  
   return (
-         <>
-         <button className="btn" onclick="activateLasers()">Clicked n times</button>
-         </>
+    <>
+      <button className="btn" onClick={handleClick}>Clicked {clickCount} times</button>
+    </>
   );
 }
 
