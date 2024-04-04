485c1e826167edf98ff108de16aa6f6ec1b1a70e
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-04-04 10:34:53 +0000

    instalation of app reacte

--- TP/index.js
+++ TP/index.js
@@ -1,7 +1,7 @@
 
 const { addition, multiplication } = require('./operations');
 const resultatAddition = addition(5, 3);
- fonsole.log('Résultat de l\'addition :', resultatAddition);
+ciuybbhu_ybyhf HashChangeEvent, fonsole.log('Résultat de l\'addition :', resultatAddition);
 
 const resultatMultiplication = multiplication(4, 6);
 console.log('Résultat de la multiplication :', resultatMultiplication);
