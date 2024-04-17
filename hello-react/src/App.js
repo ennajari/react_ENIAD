b43c8c9028fa86f6d6be9c9cdf037ffbbf29ba20
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-04-17 11:47:02 +0100

    new project

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -9,15 +9,10 @@ function App() {
   };
   
   return (
-<<<<<<< HEAD
     <>
       <button className="btn" onClick={handleClick}>Clicked {clickCount} times</button>
     </>
-=======
-         <>
-         <button className="btn" onclick="activateLasers()">Clicked ${n} times</button>
-         </>
->>>>>>> 13b7b22044facde4e4d0d3d7fba0e8da102758b7
   );
 }
+
 export default App;
