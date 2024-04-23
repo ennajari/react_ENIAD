89c34eee715e2307d810d59cf148b8c9ea08f3af
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-04-23 22:45:40 +0100

    Search btn

--- tp2/src/App.js
+++ tp2/src/App.js
@@ -1,8 +1,12 @@
+import React, { useState } from 'react';
 import './App.css';
 
-const Product = ({ product }) => {
-  const addToCart = () => {
-    console.log(`Ajouter au panier: ${product.title}`);
+const Product = ({ product, addToCart }) => {
+  const [addedToCart, setAddedToCart] = useState(false);
+
+  const handleAddToCart = () => {
+    addToCart(product);
+    setAddedToCart(true);
   };
 
   return (
@@ -10,22 +14,34 @@ const Product = ({ product }) => {
       <img src={product.thumbnail} alt={product.title} />
       <h2>{product.title}</h2>
       <p>Prix: {product.price} DH</p>
-      <button onClick={addToCart}>Ajouter au panier</button>
+      {addedToCart ? (
+        <p>Ajouté au panier</p>
+      ) : (
+        <button onClick={handleAddToCart}>Ajouter au panier</button>
+      )}
     </div>
   );
 };
 
-const Products = ({ products }) => {
+const Products = ({ products, addToCart }) => {
   return (
     <div className="p2">
       {products.map(product => (
-        <Product key={product.id} product={product} />
+        <Product key={product.id} product={product} addToCart={addToCart} />
       ))}
     </div>
   );
 };
 
-const products = [ 
+const Search = ({ handleSearch }) => {
+  return (
+    <div>
+      <input type="text" id='Search' placeholder="Rechercher par titre..." onChange={e => handleSearch(e.target.value)} />
+    </div>
+  );
+};
+
+const productsData = [ 
   { id: 1, title: 'PC Portable Gamer HP VICTUS', price: '7490 DH', thumbnail : 'HP16D0195NF.jpg' }, 
   { id: 2, title: 'PC Portable Gamer HP VICTUS', price: '2190 DH', thumbnail : 'HP14424U3EA.jpg' }, 
   { id: 3, title: 'Pc Portable Chromebook Acer', price: '3640 DH', thumbnail: 'NXATHEF002.jpg' }, 
@@ -33,9 +49,26 @@ const products = [
 ];
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const addToCart = product => {
+    setCart([...cart, product]);
+  };
+
+  const handleSearch = term => {
+    setSearchTerm(term);
+  };
+
+  const filteredProducts = productsData.filter(product =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <Products products={products} />
+      <Search handleSearch={handleSearch} />
+      <Products products={filteredProducts} addToCart={addToCart} />
+      <p>Nombre de produits dans le panier : {cart.length}</p>
     </div>
   );
 }
